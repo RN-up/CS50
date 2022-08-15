6bb17a729396b59d7cc3df496324de4b355e694a
RN-up <111278390+RN-up@users.noreply.github.com>
2022-08-15 18:46:00 +0800

    Update hello.c

--- Problem set 1/hello.c
+++ Problem set 1/hello.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
+#include <cs50.h>
 
+int main(void)
+{
+
+   string name = get_string("what is your name?\n"); // string here is a variable type
+   printf("hello, %s\n", name);
+}
